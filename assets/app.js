b8f5d7ccdba3aba309771e651c4b0fb3842b1bc2
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2017-04-10 15:32:56 -0400

    Made a lot of changes to the portfolio page. Added some JS to make things fancier and a carousel to make portfolio page simpler.

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,52 @@
-console.log("Running!");
+$(document).ready(function(){
+	$(".carousel").carousel();
+
+	$(".carousel-caption").hide()
+
+	$(".item").hover(function(){
+		$(".carousel-caption").show()
+	}, 
+	function() {
+		$(".carousel-caption").hide()
+	});
+
+	var formSub = {
+		form: $("#contact"),
+		formValidation: function(){
+			if (name === " ") {
+				alert("Please enter your name.");
+			} else if (email === " ") {
+				alert("Please enter your email address.");
+			} else if (message === " ") {
+				alert("Please enter a message");
+			} else {
+				return;
+			}
+		},
+		clear: function(){
+			$("#name").val("");
+			$("#email").val("");
+			$("#message").val("");
+		}
+	}
+
+	$("#submit").on("click", function(e){
+		e.preventDefault();
+		formSub.formValidation();
+
+		var formData = $(formSub.form).serialize();
+
+		$.ajax({
+			type: "POST",
+			url: $(formSub.form).attr("action"),
+			data: formData
+		}).done(function(data){
+			$(".modal").modal();
+			formSub.clear();
+		}).fail(function(data){
+			console.log(data);
+			alert("Oops! It looks like an error occurred and your message wasn't sent.");
+		});
+	});
+
+});
