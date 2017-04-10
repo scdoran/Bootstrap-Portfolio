8032d6f3cd6a25ec04ee7af0eee61fb0cd8b423f
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2017-04-10 16:45:12 -0400

    The mailer.php is not giving me any more errors, but it looks like I cannot receive emails. Added some css stuff for the modal.

--- mailer.php
+++ mailer.php
@@ -11,7 +11,6 @@
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail Sent. Thank you " . $first_name . ", I will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
 
     // // Only process POST reqeusts.
