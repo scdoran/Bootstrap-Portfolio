a0f86203ac7c7c9a1565cda2f7492a2d02b20809
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2017-04-13 14:17:36 -0400

    See if it will work now.

--- mailer.php
+++ mailer.php
@@ -4,33 +4,21 @@
     require("./sendgrid-php/sendgrid-php.php");
 
 
-    // if(isset($_POST['submit'])){
-    // $to = "dorans424@gmail.com";
-    // $from = $_POST['email'];
-    // $name = $_POST['name'];
-    // $subject = "Form submission";
-    // $subject2 = "Form submission";
-    // $message = $_POST['name'] . " wrote the following:" . "\n\n" . $_POST['message'];
-    // $message2 = $first_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    if(isset($_POST['submit'])){
+    $to = "dorans424@gmail.com";
+    $from = $_POST['email'];
+    $name = $_POST['name'];
+    $subject = "Form submission";
+    $subject2 = "Form submission";
+    $message = $_POST['name'] . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = $first_name . " wrote the following:" . "\n\n" . $_POST['message'];
 
-    // $headers = "From:" . $from;
-    // $headers2 = "From:" . $to;
-    // mail($to,$subject,$message,$headers);
-    // mail($from,$subject2,$message2,$headers2);
-    // echo "Mail Sent. Thank you " . $first_name . ", I will contact you shortly.";
-    // }
-
-    $from = new SendGrid\Email("Example User", "test@example.com");
-    $subject = "Sending with SendGrid is Fun";
-    $to = new SendGrid\Email("Example User", "test@example.com");
-    $content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
-    $mail = new SendGrid\Mail($from, $subject, $to, $content);
-    $apiKey = getenv('SENDGRID_API_KEY');
-    $sg = new \SendGrid($apiKey);
-    $response = $sg->client->mail()->send()->post($mail);
-    echo $response->statusCode();
-    echo $response->headers();
-    echo $response->body();
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2);
+    echo "Thank you " . $first_name . ", I will contact you shortly.";
+    }
 
     // $from = new SendGrid\Email($_POST['email']);
     // $subject = "Email from $_POST['name']";
